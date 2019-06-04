fede6a06bc355cdf85c2510e5a3b6bfedd65924d
Ryo Aoyama <r.fe51028.r@gmail.com>
2019-06-04 23:39:11 +0900

    Initial Commit

---
+++ SwiftUI-Combine-Example/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI-Combine-Example
+//
+//  Created by 青山 遼 on 2019/06/04.
+//  Copyright © 2019 Ryo Aoyama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
